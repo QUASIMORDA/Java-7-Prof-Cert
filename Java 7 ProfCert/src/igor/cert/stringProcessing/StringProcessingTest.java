538d8d4e36339916e2e58904583faf0412b3dadd
Igor Heinrich <hiwww2001@yahoo.de>
2014-12-15 21:53:19 +0100

    String pattern matchin und formatting added

--- Java 7 ProfCert/src/igor/cert/stringProcessing/StringProcessingTest.java
+++ Java 7 ProfCert/src/igor/cert/stringProcessing/StringProcessingTest.java
@@ -1,5 +1,8 @@
 package igor.cert.stringProcessing;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringProcessingTest {
 
 	public static void main(String[] args) {
@@ -23,6 +26,38 @@ public class StringProcessingTest {
 		System.out.println("Double from string: " + Double.parseDouble("12345.8907"));
 		
 		// Pattern Matching
+		
+		String str2 = "Igor, Jula, Stern";
+		for(String el:str2.split(", ") ){
+			System.out.println("'" + el + "'");
+		}
+		
+		// RegExps
+		String str3 = "123 mama 456 papa hallo 23";
+		Pattern pattern1 = Pattern.compile("\\D+\\s");
+		Matcher matcher1 = pattern1.matcher(str3);
+		while(matcher1.find()){
+			System.out.println(matcher1.group());
+		}
+		
+		Pattern pattern2 = Pattern.compile("\\d{2,}");
+		Matcher matcher2 = pattern2.matcher(str3);
+		String res = matcher2.replaceAll("XXX");
+		System.out.println(res);
+		
+		System.out.println(Pattern.matches(".+mama 456.+", str3));
+		
+		// String formatting
+		System.out.printf("%-10s%n", "igor");
+		System.out.printf("%010d\n", 17);
+		System.out.printf("%2.3f\n", 17.1236567);
+		System.out.printf("%5.3f\n", 17.1);
+		System.out.printf("%5d\n", 17);
+		System.out.printf("%o\t%x\t%h\n", 17, 17, 17);
+		//System.out.printf("%5d\n"); MissingFormatArgumentException
+		String formattedString = String.format("%-10s", "igor");
+		System.out.println("'" + formattedString + "'");
+		
 	}
 
 }
